a466c663c70975b008a14d4cd7b776ce77e7db47
Coyotefrank <franklopez35789@gmail.com>
2023-08-29 22:11:33 -0700

    created the starter code and ran the vite

---
+++ react-profile/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+import "./styles.css"
+
+export default function App(){
+ useState("")
+  return <>
+ <header className="header1">Francisco Jimenez</header>
+
+  </>
+}
