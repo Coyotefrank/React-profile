b2605773e7809a0c24b6248134b65f42ef42e120
Coyotefrank <franklopez35789@gmail.com>
2023-08-30 21:16:22 -0700

    added images and some html comtent for the page, also added links to my githun linkedin and my email

--- react-profile/src/App.jsx
+++ react-profile/src/App.jsx
@@ -1,10 +1,39 @@
-import { useState } from "react"
-import "./styles.css"
+import { useState } from "react";
+import "./styles.css";
 
-export default function App(){
- useState("")
-  return <>
- <header className="header1">Francisco Jimenez</header>
-
-  </>
+export default function App() {
+  const [bio, setBio] = useState("Software Developer");
+  
+  return (
+    <div className="profile-container">
+      <header className="header1">Francisco Jimenez</header>
+      <div className="profile-picture">
+        <img 
+        src="../public/Profile pic2.jpg"
+        alt="profile picture"
+        height="300"
+        width="200"
+        
+        />
+      </div>
+      <div className="bio">
+        <p>{bio}</p>
+        <button onClick={() => setBio("Father, Husband, and future developer")}>
+          Update Bio
+        </button>
+      
+      {/* Add more sections like work experience, education, skills, etc. */}
+    </div>
+    <div className="social-links">
+    <a href="https://github.com/Coyotefrank" target="_blank" rel="noopener noreferrer">
+      GitHub
+    </a>
+    <a href="https://www.linkedin.com/in/frank-jimenez-691568141/" target="_blank" rel="noopener noreferrer">
+      LinkedIn
+    </a>
+  <a href="mailto:your-Franklopez35789@gmail.com">Email</a>
+  </div>
+ 
+</div>
+  );
 }
